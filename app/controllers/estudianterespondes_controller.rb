7dc2a2c9d4de0e57d89d920de4bc64a8adb9c890
developer codecastle <developer@developer-HP-Compaq-dx2400-Microtower-PC.(none)>
2016-11-29 14:18:13 -0600

    Working on evaluation module

--- app/controllers/estudianterespondes_controller.rb
+++ app/controllers/estudianterespondes_controller.rb
@@ -2,11 +2,26 @@ class EstudianterespondesController < ApplicationController
   
   def new
     @estudianteresponde = EstudianteResponde.new
-    @preguntas = Pregunta.where(evaluacion_id: params[:evaluacion_id]).count
-    
+    @cpreguntas = Pregunta.where(evaluacion_id: params[:evaluacion_id]).count
+    @preguntas = Pregunta.where(evaluacion: params[:evaluacion_id]).select("id")
+    @respuestasCorrectas = Respuesta.where(correcta: true, pregunta_id: @preguntas).select("id")
+    @correctas = 0
+    @respuestas = params[:respuestas]
+    @respuestasCorrectas.each do |rc|
+     @respuestas.each do |r|
+        if rc.id==r.to_i
+         @correctas = @correctas + 1
+        end
+      end
+    end
+    @evaluacionestudiante = EvaluacionEstudiante.where(user_id: params[:user_id], evaluacion_id: params[:evaluacion_id])
+    @nota = @correctas.to_d/@cpreguntas*10
+    @evaluacionestudiante.update(@evaluacionestudiante.ids[0],nota: @nota)
+    #@evaluacionestudiante.ids
     #params[:respuestas].each do |r|
      # EstudianteResponde.create(user_id: params[:user_id],pregunta_id: params[:pregunta_id],respuesta: r)
     #end
+    #@correctas.to_d/@cpreguntas*10
   end
   
 end
